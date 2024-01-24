11dc51b7aa1b4af82c1757a1dd811e5ff9cd6b16
tlgusdl03 <tlgusdl03@gmail.com>
2024-01-24 17:25:44 +0900

    fix: 지도, polyline, marker 출력, polyline 패턴 변경

--- lib/map/screen/home_screen.dart
+++ lib/map/screen/home_screen.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
-import 'package:google_map_proj/const/data.dart';
 import 'package:dio/dio.dart';
 
-
+//서버로부터 받은 polyline을 decode한 결과
 final List<PointLatLng> results = PolylinePoints().decodePolyline(
     '{wqcFov`dW??RFXHDBb@L????YbB????l@T????RF????AFu@xDSbB????DB????EC????RcBt@yD@G????SG????m@U????XcB????c@MECYISG??');
+
+// PointLatLng 형식의 리스트 results를 LatLng 형식의 리스트로 변환
 final List<LatLng> resultList = results.map((point) {
   return LatLng(point.latitude, point.longitude);
 }).toList();
@@ -21,16 +22,23 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late GoogleMapController mapController;
+  //polyline들의 집합
   Set<Polyline> polylines = {};
+  //marker들의 집합
   Set<Marker> markers = {};
 
   @override
   void initState() {
-    getinformation();
+    //getinformation();
+    drawPolylines();
+    drawMarkers();
     super.initState();
   }
 
+  //새롭게 경로와 마커를 그릴 때 마다 기존의 경로와 마커를 지우고 새것을 생성함
   drawPolylines() async {
+    polylines.clear();
+
     polylines.add(
       Polyline(
         polylineId: PolylineId(
@@ -40,12 +48,15 @@ class _HomeScreenState extends State<HomeScreen> {
         width: 5,
         points: resultList,
         color: Colors.blue,
+        patterns: [PatternItem.dash(20), PatternItem.gap(20)],
       ),
     );
     setState(() {});
   }
 
   drawMarkers() async {
+    markers.clear();
+
     markers.add(
       Marker(
         markerId: MarkerId("origin"),
@@ -71,23 +82,23 @@ class _HomeScreenState extends State<HomeScreen> {
     return resultList;
   }
 
-  getinformation() async {
-    final dio = Dio();
-    final resp = await dio.get(
-      'ip 주소 입력',
-      options: Options(
-        headers: {
-          'authorization': 'Bearer $accessToken',
-        },
-      ),
-    );
-    final before_convert = PolylinePoints().decodePolyline(resp.data);
-    final after_convert = before_convert.map((point) {
-      return LatLng(point.latitude, point.longitude);
-    }).toList();
-    drawPolylines();
-    drawMarkers();
-  }
+  // getinformation() async {
+  //   final dio = Dio();
+  //   final resp = await dio.get(
+  //     'ip 주소 입력',
+  //     options: Options(
+  //       headers: {
+  //         'authorization': 'Bearer $accessToken',
+  //       },
+  //     ),
+  //   );
+  //   final before_convert = PolylinePoints().decodePolyline(resp.data);
+  //   final after_convert = before_convert.map((point) {
+  //     return LatLng(point.latitude, point.longitude);
+  //   }).toList();
+  //   drawPolylines();
+  //   drawMarkers();
+  // }
 
   @override
   Widget build(BuildContext context) {
