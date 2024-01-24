c1830586118324198f7a34da41873a02a7c7bd76
tlgusdl03 <tlgusdl03@gmail.com>
2024-01-24 17:26:49 +0900

    fix: RouteModel 수정

--- lib/map/model/route_model.dart
+++ lib/map/model/route_model.dart
@@ -1,21 +1,33 @@
-class RouteModel{
-  final List<Coordinates> coordinates;
+// class RouteModel{
+//   final List<Coordinates> coordinates;
+//   final double totalTimeSeconds;
+//   final double totalDistanceMeters;
+//
+//   RouteModel({
+//     required this.coordinates,
+//     required this.totalTimeSeconds,
+//     required this.totalDistanceMeters,
+//   });
+// }
+//
+// class Coordinates{
+//   final double latitude;
+//   final double longitude;
+//
+//   Coordinates({
+//     required this.latitude,
+//     required this.longitude,
+//   });
+// }
+
+class RouteModel {
+  final String Encoded_Polyline;
   final double totalTimeSeconds;
   final double totalDistanceMeters;
 
   RouteModel({
-    required this.coordinates,
+    required this.Encoded_Polyline,
     required this.totalTimeSeconds,
     required this.totalDistanceMeters,
-  });
-}
-
-class Coordinates{
-  final double latitude;
-  final double longitude;
-
-  Coordinates({
-    required this.latitude,
-    required this.longitude,
   });
 }
