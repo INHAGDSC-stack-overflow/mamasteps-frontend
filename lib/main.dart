dd5fa310f02501dd677d5b0ceb4949865d88bab2
tlgusdl03 <tlgusdl03@gmail.com>
2024-02-03 18:51:01 +0900

    feat: 날짜 입력기능 추가, 산책 선호 시간 기능 추가중

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:calendar_view/calendar_view.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mamasteps_frontend/login/widget/google_login_components.dart';
@@ -11,10 +12,13 @@ void main() async {
       onTap: () {
         FocusManager.instance.primaryFocus?.unfocus();
       },
-      child: MaterialApp(
-        // 2번코드
-        debugShowCheckedModeBanner: false,
-        home: SignUpPage(),
+      child: CalendarControllerProvider(
+        controller: EventController(),
+        child: MaterialApp(
+          // 2번코드
+          debugShowCheckedModeBanner: false,
+          home: SignUpPage(),
+        ),
       ),
     ),
   );
--- lib/ui/screen/sign_up_page.dart
+++ lib/ui/screen/sign_up_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:mamasteps_frontend/ui/data/contents.dart';
+import 'package:numberpicker/numberpicker.dart';
+import 'package:calendar_view/calendar_view.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -33,8 +35,7 @@ class _SignUpPageState extends State<SignUpPage> {
                   physics: NeverScrollableScrollPhysics(),
                   controller: _pageController,
                   children: [
-                    _subPage(
-                      content: contents[0],
+                    _subPage0(
                       onChanged: onNameChanged,
                     ),
                     _subPage(
@@ -50,6 +51,10 @@ class _SignUpPageState extends State<SignUpPage> {
                       onChanged: onActivitesChanged,
                     ),
                     _subpage2(),
+                    _subpage3(),
+                    _subPage4(
+                      onChanged: onNameChanged,
+                    ),
                   ],
                 ),
               ),
@@ -119,8 +124,8 @@ class _SignUpPageState extends State<SignUpPage> {
 
 class _subPage extends StatelessWidget {
   final ValueChanged onChanged;
-
   final String content;
+
   const _subPage({
     super.key,
     required this.content,
@@ -134,6 +139,56 @@ class _subPage extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Text(content),
+        const SizedBox(height: 16.0),
+        TextFormField(
+          autofocus: true,
+          onChanged: onChanged,
+        ),
+      ],
+    );
+  }
+}
+
+class _subPage0 extends StatelessWidget {
+  final ValueChanged onChanged;
+
+  const _subPage0({
+    super.key,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(contents[0]),
+        const SizedBox(height: 16.0),
+        TextFormField(
+          autofocus: true,
+          onChanged: onChanged,
+        ),
+      ],
+    );
+  }
+}
+
+class _subPage1 extends StatelessWidget {
+  final ValueChanged onChanged;
+
+  const _subPage1({
+    super.key,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(contents[0]),
         const SizedBox(height: 16.0),
         TextFormField(
           autofocus: true,
@@ -167,7 +222,7 @@ class _subpage2State extends State<_subpage2> {
           controller: _dateController,
           readOnly: true,
           onTap: () async {
-            showDatePicker(
+            await showDatePicker(
               context: context,
               initialDate: date,
               firstDate: DateTime(2000),
@@ -183,6 +238,200 @@ class _subpage2State extends State<_subpage2> {
           },
         ),
       ],
+    );
+  }
+}
+
+class _subpage3 extends StatefulWidget {
+  const _subpage3({
+    super.key,
+  });
+
+  @override
+  State<_subpage3> createState() => _subpage3State();
+}
+
+class _subpage3State extends State<_subpage3> {
+  int _hour = 0;
+  int _minute = 0;
+
+  // Future<void> _showTimePickerDialog() async {
+  //   await showDialog(context: context, builder: (BuildContext context){
+  //     return AlertDialog(
+  //       title: Text('시간을 선택해 주세요'),
+  //       content: Container(
+  //         child: Row(
+  //           mainAxisSize: MainAxisSize.min,
+  //           children: [
+  //             NumberPicker(
+  //               value: _hour,
+  //               minValue: 0,
+  //               maxValue: 23,
+  //               onChanged: (value) => setState(() => _hour = value),
+  //             ),
+  //             NumberPicker(
+  //               value: _minute,
+  //               minValue: 0,
+  //               maxValue: 59,
+  //               onChanged: (value) => setState(() => _minute = value),
+  //             ),
+  //           ],
+  //         ),
+  //       ),
+  //       actions: <Widget>[
+  //         TextButton(
+  //           child: Text('확인'),
+  //           onPressed: () {
+  //             Navigator.of(context).pop();
+  //             _dateController.text = '$_hour시간 $_minute분';
+  //           }
+  //         )
+  //       ]
+  //     );
+  //   });
+  // }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(contents[3]),
+        const SizedBox(height: 16.0),
+        Container(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              NumberPicker(
+                value: _hour,
+                minValue: 0,
+                maxValue: 23,
+                onChanged: (value) => setState(() => _hour = value),
+              ),
+              Text("시간"),
+              NumberPicker(
+                value: _minute,
+                minValue: 0,
+                maxValue: 59,
+                onChanged: (value) => setState(() => _minute = value),
+              ),
+              Text("분"),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _subPage4 extends StatelessWidget {
+  final ValueChanged onChanged;
+
+  const _subPage4({
+    super.key,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(contents[0]),
+        const SizedBox(height: 16.0),
+        Expanded(
+          child: WeekView(
+            controller: EventController(),
+            eventTileBuilder: (date, events, boundry, start, end) {
+              return Container();
+            },
+            fullDayEventBuilder: (events, date) {
+              return Container();
+            },
+            width: MediaQuery.of(context).size.width * 0.8,
+            minDay: DateTime.now(),
+            maxDay: DateTime.now(),
+            initialDay: DateTime.now(),
+            heightPerMinute: 1,
+            eventArranger: SideEventArranger(),
+            onEventTap: (events, date) => print(events),
+            onDateLongPress: (date) => print(date),
+            onDateTap: (DateTime date) async {
+              await _showMyDialog(context);
+            },
+            startDay: WeekDays.sunday,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future<void> _showMyDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+          title: Text('요일을 선택해 주세요'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                ListTile(
+                  title: Text('월요일'),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  title: Text('화요일'),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  title: Text('수요일'),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  title: Text('목요일'),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  title: Text('금요일'),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  title: Text('토요일'),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  title: Text('일요일'),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
