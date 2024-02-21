4bd22e056f4b8d86ceba71ceaa9f8209fd15833b
tlgusdl03 <tlgusdl03@gmail.com>
2024-02-22 00:34:43 +0900

    feat: 구글 캘린더, get, insert 테스트 중

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:mamasteps_frontend/calendar/component/google_calendar.dart';
 import 'package:mamasteps_frontend/login/screen/login_page.dart';
 import 'package:mamasteps_frontend/login/screen/login_page.dart';
 import 'package:mamasteps_frontend/map/screen/map_page.dart';
@@ -12,8 +13,13 @@ import 'package:mamasteps_frontend/ui/screen/root_tab.dart';
 import 'package:mamasteps_frontend/ui/screen/sign_up_page.dart';
 import 'package:mamasteps_frontend/ui/screen/splash_screen.dart';
 import 'package:intl/date_symbol_data_local.dart' as localedate;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+
+String apikey = dotenv.env['myClientId'].toString();
 void main() async {
+  await dotenv.load(fileName: ".env");
+  print(apikey);
   await localedate.initializeDateFormatting();
   runApp(
     GestureDetector(
@@ -23,7 +29,7 @@ void main() async {
       child: MaterialApp(
         // 2번코드
         debugShowCheckedModeBanner: false,
-        home: SplashScreen(),
+        home: SignInDemo(),
       ),
     ),
   );
